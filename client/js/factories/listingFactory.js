f1201ea004b0359264604c65eec7dba44eb1bad7
Porthos <leedeffebach@gmail.com>
2018-11-09 13:32:13 -0500

    trying to get heroku to work

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,20 +1,20 @@
 angular.module("listings", []).factory("Listings", function($http) {
   var methods = {
     getAll: function() {
-      return $http.get("https://group6-uf-web-app.herokuapp.com:8080/api/listings");
+      return $http.get("https://group6-uf-web-app.herokuapp.com/api/listings");
     },
 
     create: function(listing) {
-      return $http.post("https://group6-uf-web-app.herokuapp.com:8080/api/listings", listing);
+      return $http.post("https://group6-uf-web-app.herokuapp.com/api/listings", listing);
     },
 
     delete: function(id) {
-      return $http.post("https://group6-uf-web-app.herokuapp.com:8080/api/listings", id);
+      return $http.post("https://group6-uf-web-app.herokuapp.com/api/listings", id);
     },
 
     login: function(login_listing) {
       return $http.post(
-        "https://group6-uf-web-app.herokuapp.com:8080/api/listings/login",
+        "https://group6-uf-web-app.herokuapp.com/api/listings/login",
         login_listing
       );
     },
@@ -27,7 +27,7 @@ angular.module("listings", []).factory("Listings", function($http) {
     },
 
     getEditable: function() {
-      return $http.get("https://group6-uf-web-app.herokuapp.com:8080/api/professor-info");
+      return $http.get("https://group6-uf-web-app.herokuapp.com/api/professor-info");
     }
 
   };

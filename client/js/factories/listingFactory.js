476b4a62915b6cd286f041377f5c99499056ff3e
Porthos <leedeffebach@gmail.com>
2018-11-09 13:27:29 -0500

    more heroku

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,33 +1,33 @@
 angular.module("listings", []).factory("Listings", function($http) {
   var methods = {
     getAll: function() {
-      return $http.get("https://group6-uf-web-app.herokuapp.com/:8080/api/listings");
+      return $http.get("https://group6-uf-web-app.herokuapp.com:8080/api/listings");
     },
 
     create: function(listing) {
-      return $http.post("https://group6-uf-web-app.herokuapp.com/:8080/api/listings", listing);
+      return $http.post("https://group6-uf-web-app.herokuapp.com:8080/api/listings", listing);
     },
 
     delete: function(id) {
-      return $http.post("https://group6-uf-web-app.herokuapp.com/:8080/api/listings", id);
+      return $http.post("https://group6-uf-web-app.herokuapp.com:8080/api/listings", id);
     },
 
     login: function(login_listing) {
       return $http.post(
-        "https://group6-uf-web-app.herokuapp.com/api/listings/login",
+        "https://group6-uf-web-app.herokuapp.com:8080/api/listings/login",
         login_listing
       );
     },
 
     update: function(editListing) {
       return $http.post(
-        "https://group6-uf-web-app.herokuapp.com/api/professor-info/update",
+        "https://group6-uf-web-app.herokuapp.com:8080/api/professor-info/update",
         editListing
       );
     },
 
     getEditable: function() {
-      return $http.get("https://group6-uf-web-app.herokuapp.com/api/professor-info");
+      return $http.get("https://group6-uf-web-app.herokuapp.com:8080/api/professor-info");
     }
 
   };

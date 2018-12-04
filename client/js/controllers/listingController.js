91581852427c16deb4fa49a5e3f0235d8057cd30
Fernando Quintero <ferdanielq@outlook.com>
2018-12-03 19:15:51 -0500

    added page navigation to courses

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,4 +1,4 @@
-const dash_addr = 'https://group6-uf-web-app.herokuapp.com/protected/dashboard'
+const dash_addr = "https://group6-uf-web-app.herokuapp.com/protected/dashboard";
 //const dash_addr = 'http://localhost:8080/protected/dashboard'
 
 angular.module("listings").controller("ListingsController", [
@@ -6,43 +6,44 @@ angular.module("listings").controller("ListingsController", [
   "Listings",
   "$http",
   "$sce",
-  function ($scope, Listings, $http, $sce) {
-
+  function($scope, Listings, $http, $sce) {
     $scope.marker = null;
 
     const provider = new window.GeoSearch.OpenStreetMapProvider();
 
     const searchControl = new window.GeoSearch.GeoSearchControl({
-      provider: provider,
+      provider: provider
     });
 
-    const map = new window.L.Map('map');
+    const map = new window.L.Map("map");
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18
     }).addTo(map);
 
     map.addControl(searchControl);
-    map.setView([29.651940, -82.324996], 13);
+    map.setView([29.65194, -82.324996], 13);
 
+    $scope.data = [];
     Listings.getCourses().then(
-      function (response) {
+      function(response) {
         $scope.data = response.data;
         //console.log("response.data: " + JSON.stringify(response.data));
       },
-      function (error) {
+      function(error) {
         console.log("Unable to retrieve listings:", error);
       }
     );
 
     /* Get all the listings, then bind it to the scope */
     Listings.getAll().then(
-      function (response) {
+      function(response) {
         $scope.listings = response.data;
         //console.log("response.data: " + response.data);
       },
-      function (error) {
+      function(error) {
         console.log("Unable to retrieve listings:", error);
       }
     );
@@ -50,36 +51,36 @@ angular.module("listings").controller("ListingsController", [
     $scope.detailedInfo = undefined;
     $scope.detailedTwitter = undefined;
 
-    $scope.addListing = function () {
+    $scope.addListing = function() {
       /**
 	  *Save the article using the Listings factory. If the object is successfully
 	  saved redirect back to the list page. Otherwise, display the error
 	 */
       Listings.create($scope.newListing).then(
-        function (res) {
-          console.log('created');
+        function(res) {
+          console.log("created");
           window.location.replace(dash_addr);
         },
-        function (error) {
+        function(error) {
           console.log("Unable to create listing: ", error);
         }
       );
     };
 
     //check if password is valid.
-    $scope.login = function () {
+    $scope.login = function() {
       Listings.login($scope.login_listing).then(
-        function (res) {
-          console.log('logged in');
+        function(res) {
+          console.log("logged in");
           window.location.replace(dash_addr);
         },
-        function (error) {
+        function(error) {
           console.log("Unable to login: ", error);
         }
       );
     };
 
-    $scope.deleteListing = function (index) {
+    $scope.deleteListing = function(index) {
       /**TODO
         Delete the article using the Listings factory. If the removal is successful,
 		navigate back to 'listing.list'. Otherwise, display the error.
@@ -89,14 +90,14 @@ angular.module("listings").controller("ListingsController", [
     $scope.details = false;
     $scope.tweets = false;
 
-    $scope.showDetails = function (index, username) {
+    $scope.showDetails = function(index, username) {
       $scope.details = true;
 
       if (username) {
-        $http.get('twitter/' + username).then(function (resposne) {
-          console.log(resposne)
+        $http.get("twitter/" + username).then(function(resposne) {
+          console.log(resposne);
           $scope.detailedTwitter = $sce.trustAsHtml(resposne.data.html);
-        })
+        });
         $scope.tweets = true;
       } else {
         $scope.tweets = false;
@@ -104,37 +105,65 @@ angular.module("listings").controller("ListingsController", [
 
       $scope.detailedInfo = $scope.listings[index];
       if ($scope.detailedInfo.address) {
-        const results = provider.search({
-          query: $scope.detailedInfo.address
-        }).then(function (results) {
-          //console.log(results);
-          $scope.marker = window.L.popup()
-            .setLatLng([results[0].y, results[0].x])
-            .setContent($scope.detailedInfo.address)
-            .openOn(map);
-        });
+        const results = provider
+          .search({
+            query: $scope.detailedInfo.address
+          })
+          .then(function(results) {
+            //console.log(results);
+            $scope.marker = window.L.popup()
+              .setLatLng([results[0].y, results[0].x])
+              .setContent($scope.detailedInfo.address)
+              .openOn(map);
+          });
       }
     };
 
     //search functionality
     $scope.search = { courseCode: "", courseTitle: "" };
     $scope.lastSearch = { courseCode: "", courseTitle: "" };
+    $scope.startingFrom = 0;
 
-    $scope.getSearchCourses = function () {
-      $scope.lastSearch = $scope.search;
-      $scope.getNextCourses(0);
+    $scope.getSearchCourses = function() {
+      $scope.startingFrom = 0;
+      $scope.getCourses();
+    };
+
+    $scope.getNextCourses = function() {
+      if (!$scope.isLastPage()) {
+        $scope.startingFrom = $scope.startingFrom + 50;
+        $scope.getCourses();
+      }
+    };
+
+    $scope.getPreviousCourses = function() {
+      $scope.startingFrom = $scope.startingFrom - 50;
+      if($scope.isFirstPage()) {
+        $scope.startingFrom = 0;
+      }
+      $scope.getCourses();
+    };
+
+    $scope.isLastPage = function() {
+      return $scope.data.RETRIEVEDROWS !== 50;
+    }
+    $scope.isFirstPage = function() {
+      return $scope.startingFrom <= 0;
     }
 
-    $scope.getNextCourses = function(startingFrom) {
+    $scope.getCourses = function() {
       Listings.getCourses({
         courseCode: $scope.search.courseCode,
         courseTitle: $scope.search.courseTitle,
-        startingFrom
-      }).then(res => {
+        startingFrom: $scope.startingFrom
+      }).then(
+        res => {
           $scope.data = res.data;
-        }, err => {
+        },
+        err => {
           console.log("Unable to retrieve listings:", err);
-        }); 
+        }
+      );
     };
   }
 ]);
